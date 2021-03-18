f8208b8844df0019932fdc9f27cff1b0440be337
Josh <j.j.h.lindner@gmail.com>
2021-03-17 18:59:52 -0700

    Added process for finding RegEx matches for phone numbers and email addresses, then copying them to the clipboard.

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -20,9 +20,28 @@ emailRegex= re.compile(r'''(
         (\.[a-zA-Z]{2,4})                    #top level domain
 )''',re.VERBOSE)
 
-#TODO: Find matches in clipboard text.
+
+#Find matches in clipboard text.
 text= str(pyperclip.paste())
+
+matches = []
+for groups in phoneRegex.findall(text):
+        phoneNum +="-".join([groups[1],groups[3],groups[5]])
+        if groups[8] != '':
+                phoneRegex+= ' x'+groups[8]
+        matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+        matches.append(groups[0])
+
 
 
 
 #TODO: Copy results to the clipboard.
+if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Copied to clipboard:')
+        print('\n'.join(matces))
+else:
+        print("No phone numbers or email adderesses found.")
+
+        
